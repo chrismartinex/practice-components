edbc6030627273fb24e057b1c259de377e61585f
chris martinez <uschrismartinez@gmail.com>
2023-05-28 17:11:53 -0700

    styles

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,10 +1,22 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-warning',
-    templateUrl: './warning.component.html'
+    template:`
+    <p>This is the warning component, you are in danger</p>
+    `,
+    styles: [
+        `
+        p {
+            padding: 20px;
+            background-color: mistyrose;
+            border: 1px solid red;
+        }
+        `
+    ]
 })
 
 export class WarningComponent {
-    
+
 }
